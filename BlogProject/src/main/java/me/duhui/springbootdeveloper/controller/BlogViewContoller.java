8945fead7b081ea96ff470ecf55f388fc94460b8
Duhui-Kim <dadada022863@gmail.com>
2023-06-12 13:53:07 +0900

    COMPLETE VIEW

--- BlogProject/src/main/java/me/duhui/springbootdeveloper/controller/BlogViewContoller.java
+++ BlogProject/src/main/java/me/duhui/springbootdeveloper/controller/BlogViewContoller.java
@@ -3,12 +3,14 @@ package me.duhui.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.duhui.springbootdeveloper.domain.Article;
 import me.duhui.springbootdeveloper.dto.ArticleListViewResponse;
+import me.duhui.springbootdeveloper.dto.ArticleResponse;
 import me.duhui.springbootdeveloper.dto.ArticleViewResponse;
 import me.duhui.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -36,5 +38,21 @@ public class BlogViewContoller {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    // id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        ArticleViewResponse articleViewResponse = null;
+
+        if(id == null) {
+            articleViewResponse = new ArticleViewResponse();
+        } else {
+            articleViewResponse = new ArticleViewResponse(blogService.findById(id));
+        }
+
+        model.addAttribute("article", articleViewResponse);
+
+        return "newArticle";
     }
 }
